7706fa857be83d2db50d4463f645cab79ad1af01
Aryaman <arygoenka@gmail.com>
2025-02-11 17:43:27 +0100

    fixing build erros

--- components/HomeFAQ.tsx
+++ components/HomeFAQ.tsx
@@ -1,5 +1,5 @@
 
-const HomeFAQ = ({titleofFAQ, textofFAQ} : {titleofFAQ:string, textofFAQ:string}) => {
+const HomeFAQ = () => {
   return (
     <div className="max-w-4xl mx-auto px-6 py-12">
       {/* Title Section */}
