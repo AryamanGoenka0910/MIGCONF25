e63361c94c6c35f5ba221556935910a02fa1a02d
Aryaman Goenka <arygoenka@gmail.com>
2026-02-27 22:31:56 -0500

    adding speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google"
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import NavBar from "@/components/Navbar";
 
@@ -55,6 +56,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={`${_geist.className} antialiased`}>
         <NavBar />
         {children}
